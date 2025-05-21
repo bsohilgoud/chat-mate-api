15c8fb9733c1f7267fbf378681a487649c7bed0d
bsohilgoud <sohilgoud@gmail.com>
2025-05-21 23:18:50 +0530

    Adds flyway integration

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,19 @@
-COPY users(display_name, username, user_id,	password, auth_provider, google_id)
-FROM 'D:\\Projects\\chat_mate_project\\chat-mate-api\\src\\main\\resources\\users.csv'
+COPY users(auth_provider,full_name,provider_id,password,user_id,username,created_at,updated_at)
+FROM 'D:\\Projects\\chat_mate_project\\data\\users.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY online_status(user_id, status, last_seen)
-FROM 'D:\\Projects\\chat_mate_project\\chat-mate-api\\src\\main\\resources\\online_status.csv'
+COPY online_status(last_seen,status, user_id)
+FROM 'D:\\Projects\\chat_mate_project\\data\\online_status.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY messages(id, sender_id, receiver_id, content, content_type, timestamp, status)
-FROM 'D:\\Projects\\chat_mate_project\\chat-mate-api\\src\\main\\resources\\messages.csv'
+COPY media(id,name,size,type,url)
+FROM 'D:\\Projects\\chat_mate_project\\data\\media.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY messages(id, timestamp, content,content_type,receiver_id,sender_id,status,media_id)
+FROM 'D:\\Projects\\chat_mate_project\\data\\messages.csv'
 DELIMITER ','
 CSV HEADER;
