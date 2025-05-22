efc925cf84ed8c516790a227232d70615b336294
bsohilgoud <sohilgoud@gmail.com>
2025-05-22 23:06:26 +0530

    updated UserDTO

--- src/main/java/com/sohil/chatmate/dto/UserDTO.java
+++ src/main/java/com/sohil/chatmate/dto/UserDTO.java
@@ -2,5 +2,6 @@ package com.sohil.chatmate.dto;
 
 import java.time.LocalDateTime;
 
-public record UserDTO(String id, String email, String username, String phoneNumber, String fullName, String profileUrl, LocalDateTime createdAt, LocalDateTime updatedAt) {
+public record UserDTO(String id, String email, String username, String fullName, String phoneNumber, String profileUrl,
+                      LocalDateTime createdAt, LocalDateTime updatedAt) {
 }
