e4e22b5eb3464166bc65d4a6998943979730dbbd
bsohilgoud <sohilgoud@gmail.com>
2025-05-24 20:00:23 +0530

    Adds UserPrinciple class

---
+++ src/main/java/com/sohil/chatmate/security/UserPrinciple.java
@@ -0,0 +1,27 @@
+package com.sohil.chatmate.security;
+
+import com.sohil.chatmate.entity.User;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class UserPrinciple {
+    private final String userID;
+    private final String username;
+    private final String email;
+    private final List<String> roles;
+    private final boolean enabled;
+
+    private UserPrinciple(String userId, String username, String email, List<String> roles, boolean enabled) {
+        this.userID = userId;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+        this.enabled = enabled;
+    }
+
+    public static UserPrinciple from(User user){
+        return new UserPrinciple(user.getUserID(), user.getUsername(), user.getEmail(), null, true);
+    }
+}
