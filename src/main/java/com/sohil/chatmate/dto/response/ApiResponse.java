5e8868dbb9f47cb475b8bbba20dabc7b22ab2b79
bsohilgoud <sohilgoud@gmail.com>
2025-06-02 17:26:36 +0530

    updated ApiResponse data key to payload

--- src/main/java/com/sohil/chatmate/dto/response/ApiResponse.java
+++ src/main/java/com/sohil/chatmate/dto/response/ApiResponse.java
@@ -1,4 +1,4 @@
-package com.sohil.chatmate.dto;
+package com.sohil.chatmate.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
@@ -18,15 +18,15 @@ import java.time.LocalDateTime;
 public class ApiResponse<T> {
     private int status;
     private String message;
-    private T data;
+    private T payload;
     private String path;
     private String timestamp;
 
     // This 1st <T> explicitly defines T as a type parameter for that method - tells Java that this method introduces its own generic type parameter T. This means T is scoped only to this method.
-    public static <T> ResponseEntity<ApiResponse<T>> success(T data, String path){
+    public static <T> ResponseEntity<ApiResponse<T>> success(T payload, String path){
         ApiResponse<T> response = new ApiResponseBuilder<T>()
                 .status(HttpStatus.SC_OK)
-                .data(data)
+                .payload(payload)
                 .timestamp(LocalDateTime.now().toString())
                 .path(path)
                 .build();
@@ -34,10 +34,10 @@ public class ApiResponse<T> {
         return ResponseEntity.ok(response);
     }
 
-    public static <T> ResponseEntity<ApiResponse<T>> success(T data, String message, String path){
+    public static <T> ResponseEntity<ApiResponse<T>> success(T payload, String message, String path){
         ApiResponse<T> response = new ApiResponseBuilder<T>()
                 .status(HttpStatus.SC_OK)
-                .data(data)
+                .payload(payload)
                 .message(message)
                 .timestamp(LocalDateTime.now().toString())
                 .path(path)
@@ -46,10 +46,10 @@ public class ApiResponse<T> {
         return ResponseEntity.ok(response);
     }
 
-    public static <T> ResponseEntity<ApiResponse<T>> success(int status, T data, String path){
+    public static <T> ResponseEntity<ApiResponse<T>> success(int status, T payload, String path){
         ApiResponse<T> response = new ApiResponseBuilder<T>()
                 .status(status)
-                .data(data)
+                .payload(payload)
                 .timestamp(LocalDateTime.now().toString())
                 .path(path)
                 .build();
@@ -57,10 +57,10 @@ public class ApiResponse<T> {
         return ResponseEntity.ok(response);
     }
 
-    public static <T> ResponseEntity<ApiResponse<T>> success(int status, T data, String message, String path){
+    public static <T> ResponseEntity<ApiResponse<T>> success(int status, T payload, String message, String path){
         ApiResponse<T> response = new ApiResponseBuilder<T>()
                 .status(status)
-                .data(data)
+                .payload(payload)
                 .message(message)
                 .timestamp(LocalDateTime.now().toString())
                 .path(path)
@@ -68,6 +68,7 @@ public class ApiResponse<T> {
 
         return ResponseEntity.ok(response);
     }
+
 
     public static <T> ApiResponse<T> error(int status, String message, String path){
         return new ApiResponseBuilder<T>()
